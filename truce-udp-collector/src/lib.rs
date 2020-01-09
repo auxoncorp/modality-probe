358080fa5a563d4663db5d097c2fa88bc05c98bf
Zachary Pierce <zack@auxon.io>
2020-01-09 15:58:05 -0600

    More proc-graph + truce integration tests

--- truce-udp-collector/src/lib.rs
+++ truce-udp-collector/src/lib.rs
@@ -454,9 +454,150 @@ mod tests {
         }
         h.join().expect("Couldn't join server handler thread");
     }
+    const TRACER_STORAGE_BYTES_SIZE: usize = 256;
+    const IN_SYSTEM_SNAPSHOT_BYTES_SIZE: usize = 256;
+    const LOG_REPORT_BYTES_SIZE: usize = 256;
 
     #[test]
-    fn pipeline_graph() {
+    fn linear_triple_inferred_unreporting_middleman_graph() {
+        let addrs = find_usable_addrs(1);
+        let server_addr = addrs[0];
+        let (shutdown_sender, shutdown_receiver) = ShutdownSignalSender::new(server_addr);
+        let (server_state_sender, server_state_receiver) = crossbeam::bounded(0);
+        let session_id = gen_session_id().into();
+        let f = tempfile::NamedTempFile::new().expect("Could not make temp file");
+        let output_file_path = PathBuf::from(f.path());
+        let config = Config {
+            addr: server_addr,
+            session_id,
+            output_file: output_file_path.clone(),
+        };
+        let h = thread::spawn(move || {
+            let mut file = std::fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(config.output_file)
+                .expect("Could not open file for writing");
+            let socket = UdpSocket::bind(config.addr).expect("Could not bind to server socket");
+            server_state_sender
+                .send(ServerState::Started)
+                .expect("Could not send status update");
+            start_receiving_from_socket(
+                socket,
+                config.session_id,
+                &mut file,
+                shutdown_receiver,
+                true,
+            );
+            let _ = server_state_sender.send(ServerState::Shutdown);
+        });
+        thread::yield_now();
+        assert_eq!(Ok(ServerState::Started), server_state_receiver.recv());
+        let mut net = proc_graph::Network::new();
+        let tracer_a_id = truce::TracerId::new(31).unwrap();
+        let tracer_b_id = truce::TracerId::new(41).unwrap();
+        let tracer_c_id = truce::TracerId::new(59).unwrap();
+        let event_foo = truce::EventId::new(7).unwrap();
+        let event_bar = truce::EventId::new(23).unwrap();
+        let event_baz = truce::EventId::new(29).unwrap();
+        const NUM_MESSAGES_FROM_A: usize = 11;
+
+        let (network_done_sender, network_done_receiver) = crossbeam::bounded(0);
+        net.add_process(
+            "a",
+            vec!["b"],
+            make_message_broadcaster_proc(
+                "a",
+                tracer_a_id,
+                NUM_MESSAGES_FROM_A,
+                server_addr,
+                Some(event_foo),
+            ),
+        );
+        net.add_process(
+            "b",
+            vec!["c"],
+            make_message_relay_proc("b", tracer_b_id, NUM_MESSAGES_FROM_A, None, Some(event_bar)),
+        );
+        net.add_process(
+            "c",
+            vec![],
+            make_message_sink_proc(
+                tracer_c_id,
+                NUM_MESSAGES_FROM_A,
+                SendLogReportEveryFewMessages {
+                    n_messages: 3,
+                    collector_addr: server_addr,
+                },
+                Some(event_baz),
+                network_done_sender,
+            ),
+        );
+        net.start();
+        thread::yield_now();
+
+        assert_eq!(Ok(()), network_done_receiver.recv());
+        shutdown_sender.shutdown();
+        assert_eq!(Ok(ServerState::Shutdown), server_state_receiver.recv());
+
+        h.join().expect("Couldn't join server handler thread");
+
+        let mut file_reader =
+            std::fs::File::open(&output_file_path).expect("Could not open output file for reading");
+        let found_log_entries = truce_analysis::read_csv_log_entries(&mut file_reader)
+            .expect("Could not read output file as csv log entries");
+
+        assert!(found_log_entries.len() > 0);
+        let expected_direct_tracer_ids: HashSet<_> = [tracer_a_id, tracer_c_id]
+            .iter()
+            .map(|id| id.get_raw())
+            .collect();
+        let built_in_event_ids: HashSet<_> = [
+            truce::LOG_OVERFLOWED,
+            truce::LOGICAL_CLOCK_OVERFLOWED,
+            truce::PRODUCED_BACKEND_LOG_REPORT,
+        ]
+        .iter()
+        .map(|id| id.get_raw())
+        .collect();
+        for e in found_log_entries {
+            assert_eq!(session_id, e.session_id);
+            assert!(expected_direct_tracer_ids.contains(&e.tracer_id.0));
+            match e.data {
+                LogEntryData::Event(event) => {
+                    // Event bar is logged only on b, and thus lost
+                    if event.0 == event_bar.get_raw() {
+                        panic!("How the heck did bar get ove there?");
+                    }
+                    if e.tracer_id.0 == tracer_a_id.get_raw() {
+                        // Process A should only be writing about event foo or the tracer internal events
+                        assert!(
+                            event.0 == event_foo.get_raw() || built_in_event_ids.contains(&event.0)
+                        );
+                    } else if e.tracer_id.0 == tracer_c_id.get_raw() {
+                        // Process C should only be writing about event baz or the tracer internals events
+                        assert!(
+                            event.0 == event_baz.get_raw() || built_in_event_ids.contains(&event.0),
+                            "unexpected event for entry: {:?}",
+                            e
+                        );
+                    }
+                }
+                LogEntryData::LogicalClock(tid, count) => {
+                    assert!(count > 0, "clock count should be > 0");
+                    if e.tracer_id.0 == tracer_a_id.get_raw() {
+                        // Process A should only know about itself, since it doesn't receive history from anyone else
+                        assert_eq!(tid.0, tracer_a_id.get_raw());
+                    } else if e.tracer_id.0 == tracer_c_id.get_raw() {
+                        // Process C should have clocks for itself and its direct precursor, B
+                        assert!(tid.0 == tracer_c_id.get_raw() || tid.0 == tracer_b_id.get_raw());
+                    }
+                }
+            }
+        }
+    }
+    #[test]
+    fn linear_pair_graph() {
         let addrs = find_usable_addrs(1);
         let server_addr = addrs[0];
         let (shutdown_sender, shutdown_receiver) = ShutdownSignalSender::new(server_addr);
@@ -495,78 +636,34 @@ mod tests {
         let tracer_b_id = truce::TracerId::new(41).unwrap();
         let event_foo = truce::EventId::new(7).unwrap();
         let event_bar = truce::EventId::new(23).unwrap();
-        const TRACER_STORAGE_BYTES_SIZE: usize = 256;
-        const IN_SYSTEM_SNAPSHOT_BYTES_SIZE: usize = 256;
-        const LOG_REPORT_BYTES_SIZE: usize = 256;
         const NUM_MESSAGES_FROM_A: usize = 11;
 
         let (network_done_sender, network_done_receiver) = crossbeam::bounded(0);
-        net.add_process("a", vec!["b"], move |id_to_sender, _receiver| {
-            let current_process_name = "a".to_string();
-            let mut tracer_storage = vec![0u8; TRACER_STORAGE_BYTES_SIZE];
-            let mut tracer = truce::Tracer::new_with_storage(&mut tracer_storage, tracer_a_id)
-                .expect("Could not make tracer");
-            let mut causal_history_blob = vec![0u8; IN_SYSTEM_SNAPSHOT_BYTES_SIZE];
-            for _ in 0..NUM_MESSAGES_FROM_A {
-                tracer.record_event(event_foo);
-                let causal_history_bytes = tracer
-                    .share_history(&mut causal_history_blob)
-                    .expect("Could not write history to share with other in-system member");
-                for destination in id_to_sender.values() {
-                    let history_copy = Vec::from(&causal_history_blob[..causal_history_bytes]);
-                    destination
-                        .send((current_process_name.clone(), history_copy))
-                        .expect("Could not send message to other process");
-                }
-            }
-
-            let socket =
-                UdpSocket::bind(OS_PICK_ADDR_HINT).expect("Could not bind to client socket");
-            let mut log_report_storage = vec![0u8; LOG_REPORT_BYTES_SIZE];
-            let log_report_bytes = tracer
-                .write_log_report(&mut log_report_storage)
-                .expect("Could not write log report");
-            socket
-                .send_to(&log_report_storage[..log_report_bytes], server_addr)
-                .expect("Could not send log report to server");
-        });
-        net.add_process("b", vec![], move |_id_to_sender, receiver| {
-            let mut tracer_storage = vec![0u8; TRACER_STORAGE_BYTES_SIZE];
-            let mut tracer = truce::Tracer::new_with_storage(&mut tracer_storage, tracer_b_id)
-                .expect("Could not make tracer");
-
-            let socket =
-                UdpSocket::bind(OS_PICK_ADDR_HINT).expect("Could not bind to client socket");
-            let mut log_report_storage = vec![0u8; LOG_REPORT_BYTES_SIZE];
-
-            let mut messages_received = 0;
-            while messages_received < NUM_MESSAGES_FROM_A {
-                let (_msg_source, message) = match receiver.recv() {
-                    Ok(m) => m,
-                    Err(std::sync::mpsc::RecvError) => {
-                        panic!("Received on a channel with no senders!")
-                    }
-                };
-                tracer
-                    .merge_history(&message)
-                    .expect("Could not merge in history");
-                tracer.record_event(event_bar);
-
-                if messages_received % 3 == 0 {
-                    let log_report_bytes = tracer
-                        .write_log_report(&mut log_report_storage)
-                        .expect("Could not write log report");
-                    socket
-                        .send_to(&log_report_storage[..log_report_bytes], server_addr)
-                        .expect("Could not send log report to server");
-                }
-                messages_received += 1;
-            }
-
-            network_done_sender
-                .send(())
-                .expect("Could not inform outside world the network is done");
-        });
+        net.add_process(
+            "a",
+            vec!["b"],
+            make_message_broadcaster_proc(
+                "a",
+                tracer_a_id,
+                NUM_MESSAGES_FROM_A,
+                server_addr,
+                Some(event_foo),
+            ),
+        );
+        net.add_process(
+            "b",
+            vec![],
+            make_message_sink_proc(
+                tracer_b_id,
+                NUM_MESSAGES_FROM_A,
+                SendLogReportEveryFewMessages {
+                    n_messages: 3,
+                    collector_addr: server_addr,
+                },
+                Some(event_bar),
+                network_done_sender,
+            ),
+        );
         net.start();
         thread::yield_now();
 
@@ -624,6 +721,180 @@ mod tests {
                     }
                 }
             }
+        }
+    }
+    use std::collections::HashMap;
+
+    fn make_message_broadcaster_proc(
+        proc_name: &'static str,
+        tracer_id: truce::TracerId,
+        n_messages: usize,
+        collector_addr: SocketAddr,
+        per_iteration_event: Option<truce::EventId>,
+    ) -> impl Fn(
+        HashMap<String, std::sync::mpsc::Sender<(String, Vec<u8>)>>,
+        std::sync::mpsc::Receiver<(String, Vec<u8>)>,
+    ) + Send
+           + 'static {
+        move |id_to_sender, _receiver| {
+            let mut tracer_storage = vec![0u8; TRACER_STORAGE_BYTES_SIZE];
+            let mut tracer = truce::Tracer::new_with_storage(&mut tracer_storage, tracer_id)
+                .expect("Could not make tracer");
+            let mut causal_history_blob = vec![0u8; IN_SYSTEM_SNAPSHOT_BYTES_SIZE];
+            for _ in 0..n_messages {
+                if let Some(e) = per_iteration_event {
+                    tracer.record_event(e);
+                }
+                let causal_history_bytes = tracer
+                    .share_history(&mut causal_history_blob)
+                    .expect("Could not write history to share with other in-system member");
+
+                for destination in id_to_sender.values() {
+                    let history_copy = Vec::from(&causal_history_blob[..causal_history_bytes]);
+                    destination
+                        .send((proc_name.to_string(), history_copy))
+                        .expect("Could not send message to other process");
+                }
+            }
+
+            let socket =
+                UdpSocket::bind(OS_PICK_ADDR_HINT).expect("Could not bind to client socket");
+            let mut log_report_storage = vec![0u8; LOG_REPORT_BYTES_SIZE];
+            let log_report_bytes = tracer
+                .write_log_report(&mut log_report_storage)
+                .expect("Could not write log report");
+            socket
+                .send_to(&log_report_storage[..log_report_bytes], collector_addr)
+                .expect("Could not send log report to server");
+        }
+    }
+
+    #[derive(Clone, Copy)]
+    struct SendLogReportEveryFewMessages {
+        n_messages: usize,
+        collector_addr: SocketAddr,
+    }
+
+    fn make_message_relay_proc(
+        proc_name: &'static str,
+        tracer_id: truce::TracerId,
+        stop_relaying_after_receiving_n_messages: usize,
+        send_log_report_every_n_messages: Option<SendLogReportEveryFewMessages>,
+        per_iteration_event: Option<truce::EventId>,
+    ) -> impl Fn(
+        HashMap<String, std::sync::mpsc::Sender<(String, Vec<u8>)>>,
+        std::sync::mpsc::Receiver<(String, Vec<u8>)>,
+    ) + Send
+           + 'static {
+        move |id_to_sender, receiver| {
+            let mut tracer_storage = vec![0u8; TRACER_STORAGE_BYTES_SIZE];
+            let mut tracer = truce::Tracer::new_with_storage(&mut tracer_storage, tracer_id)
+                .expect("Could not make tracer");
+
+            let socket =
+                UdpSocket::bind(OS_PICK_ADDR_HINT).expect("Could not bind to client socket");
+            let mut log_report_storage = vec![0u8; LOG_REPORT_BYTES_SIZE];
+
+            let mut causal_history_blob = vec![0u8; IN_SYSTEM_SNAPSHOT_BYTES_SIZE];
+            let mut messages_received = 0;
+            loop {
+                let (_msg_source, message) = match receiver.recv() {
+                    Ok(m) => m,
+                    Err(std::sync::mpsc::RecvError) => {
+                        panic!("Received on a channel with no senders!")
+                    }
+                };
+                if let Some(e) = per_iteration_event {
+                    tracer.record_event(e);
+                }
+                tracer
+                    .merge_history(&message)
+                    .expect("Could not merge in history");
+
+                if messages_received > stop_relaying_after_receiving_n_messages {
+                    continue;
+                }
+                let causal_history_bytes = tracer
+                    .share_history(&mut causal_history_blob)
+                    .expect("Could not write history to share with other in-system member");
+
+                for destination in id_to_sender.values() {
+                    let history_copy = Vec::from(&causal_history_blob[..causal_history_bytes]);
+                    destination
+                        .send((proc_name.to_string(), history_copy))
+                        .expect("Could not send message to other process");
+                }
+                if let Some(SendLogReportEveryFewMessages {
+                    n_messages,
+                    collector_addr,
+                }) = send_log_report_every_n_messages
+                {
+                    if messages_received % n_messages == 0 {
+                        let log_report_bytes = tracer
+                            .write_log_report(&mut log_report_storage)
+                            .expect("Could not write log report");
+                        socket
+                            .send_to(&log_report_storage[..log_report_bytes], collector_addr)
+                            .expect("Could not send log report to server");
+                    }
+                }
+                messages_received += 1;
+            }
+        }
+    }
+
+    fn make_message_sink_proc(
+        tracer_id: truce::TracerId,
+        stop_after_receiving_n_messages: usize,
+        send_log_report_every_n_messages: SendLogReportEveryFewMessages,
+        per_iteration_event: Option<truce::EventId>,
+        stopped_sender: crossbeam::Sender<()>,
+    ) -> impl Fn(
+        HashMap<String, std::sync::mpsc::Sender<(String, Vec<u8>)>>,
+        std::sync::mpsc::Receiver<(String, Vec<u8>)>,
+    ) + Send
+           + 'static {
+        move |_id_to_sender, receiver| {
+            let mut tracer_storage = vec![0u8; TRACER_STORAGE_BYTES_SIZE];
+            let mut tracer = truce::Tracer::new_with_storage(&mut tracer_storage, tracer_id)
+                .expect("Could not make tracer");
+
+            let socket =
+                UdpSocket::bind(OS_PICK_ADDR_HINT).expect("Could not bind to client socket");
+            let mut log_report_storage = vec![0u8; LOG_REPORT_BYTES_SIZE];
+
+            let mut messages_received = 0;
+            while messages_received < stop_after_receiving_n_messages {
+                let (_msg_source, message) = match receiver.recv() {
+                    Ok(m) => m,
+                    Err(std::sync::mpsc::RecvError) => {
+                        panic!("Received on a channel with no senders!")
+                    }
+                };
+                tracer
+                    .merge_history(&message)
+                    .expect("Could not merge in history");
+                if let Some(e) = per_iteration_event {
+                    tracer.record_event(e);
+                }
+
+                if messages_received % send_log_report_every_n_messages.n_messages == 0 {
+                    let log_report_bytes = tracer
+                        .write_log_report(&mut log_report_storage)
+                        .expect("Could not write log report");
+                    socket
+                        .send_to(
+                            &log_report_storage[..log_report_bytes],
+                            send_log_report_every_n_messages.collector_addr,
+                        )
+                        .expect("Could not send log report to server");
+                }
+                messages_received += 1;
+            }
+
+            stopped_sender
+                .send(())
+                .expect("Could not inform outside world the process is done");
         }
     }
 }
