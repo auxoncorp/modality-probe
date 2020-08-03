f8bb3ad1388034eafd61a43ad554fe2d35434404
Russell Mull <russell@auxon.io>
2020-08-03 16:14:16 -0700

    Add utility methods to the clock newtypes

--- src/lib.rs
+++ src/lib.rs
@@ -139,6 +139,24 @@ impl ProbeEpoch {
     pub const MAX: Self = ProbeEpoch(u16::MAX);
     const WRAPAROUND_THRESHOLD_TOP: Self = ProbeEpoch(u16::MAX - 3);
     const WRAPAROUND_THRESHOLD_BOTTOM: Self = ProbeEpoch(3);
+
+    /// u16::overflowing_add, on the inner value
+    pub fn overflowing_add(self, n: u16) -> (ProbeEpoch, bool) {
+        let (n, overflow) = self.0.overflowing_add(n);
+        (n.into(), overflow)
+    }
+}
+
+impl From<u16> for ProbeEpoch {
+    fn from(x: u16) -> Self {
+        ProbeEpoch(x)
+    }
+}
+
+impl From<ProbeEpoch> for u16 {
+    fn from(e: ProbeEpoch) -> Self {
+        e.0
+    }
 }
 
 /// The clock part of a probe's logical clock
@@ -157,6 +175,13 @@ impl From<u16> for ProbeTicks {
         ProbeTicks(ticks)
     }
 }
+
+impl From<ProbeTicks> for u16 {
+    fn from(t: ProbeTicks) -> Self {
+        t.0
+    }
+}
+
 
 /// Pack the epoch and clock into a u32
 #[inline]
