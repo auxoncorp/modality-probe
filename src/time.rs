d15cc6e690c08b9850de07dbd1b2a3872197f6b7
Zack Pierce <zack@auxon.io>
2021-07-09 09:05:49 -0500

    Optional serialization support derived for time types

--- src/time.rs
+++ src/time.rs
@@ -8,26 +8,46 @@
 ///
 /// This can represent approximately 73 years.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+#[cfg_attr(
+feature = "std",
+derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)
+)]
 #[repr(transparent)]
 pub struct Nanoseconds(u64);
 
 /// Nanosecond time resolution
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+#[cfg_attr(
+feature = "std",
+derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)
+)]
 #[repr(transparent)]
 pub struct NanosecondResolution(pub u32);
 
 /// Nanosecond high bits
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+#[cfg_attr(
+feature = "std",
+derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)
+)]
 #[repr(transparent)]
 pub struct NanosecondsHighBits(pub [u8; 4]);
 
 /// Nanosecond low bits
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+#[cfg_attr(
+feature = "std",
+derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)
+)]
 #[repr(transparent)]
 pub struct NanosecondsLowBits(pub [u8; 4]);
 
 /// Wall clock identifier, indicates the time domain
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+#[cfg_attr(
+feature = "std",
+derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)
+)]
 #[repr(transparent)]
 pub struct WallClockId(pub u16);
 
