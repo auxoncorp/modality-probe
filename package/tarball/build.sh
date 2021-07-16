be7d39172aa55eaaef5724df3f29d9b4cff0790f
Zack Pierce <zack@auxon.io>
2021-07-16 15:57:32 -0500

    Update workflow for collector-less world

--- package/tarball/build.sh
+++ package/tarball/build.sh
@@ -2,7 +2,6 @@
 #
 # Build dependencies:
 # * tar
-# * help2man
 # * gzip
 # * rustup component add llvm-tools-preview
 # * cargo install cross
@@ -51,61 +50,6 @@ TARBALL_FILE="${PACKAGE_NAME}.tar.gz"
     cd "$WORKSPACE_ROOT"
     rm -rf "$OUTPUT_DIR"
     mkdir -p "$OUTPUT_DIR"
-)
-
-# Build the binaries for the host, used to generate man pages and completions
-(
-    cd "$WORKSPACE_ROOT"
-
-    cargo build --release \
-        -p modality-probe-cli --bin modality-probe
-
-    cargo build --release \
-        -p modality-probe-udp-collector --bin modality-probe-udp-collector
-
-    cargo build --release \
-        -p modality-probe-debug-collector --bin modality-probe-debug-collector
-
-    cargo build --release \
-        -p modality-probe-offline-batch-collector --bin modality-probe-offline-batch-collector
-)
-
-# Build the binaries for the target
-(
-    cd "$WORKSPACE_ROOT"
-
-    cross build --release --target "$BINARY_TARGET_TRIPLE" \
-        -p modality-probe-cli --bin modality-probe
-
-    cross build --release --target "$BINARY_TARGET_TRIPLE" \
-        -p modality-probe-udp-collector --bin modality-probe-udp-collector
-
-    cross build --release --target "$BINARY_TARGET_TRIPLE" \
-        -p modality-probe-offline-batch-collector --bin modality-probe-offline-batch-collector
-
-    # Requires toolchain of target binary to be installed
-    PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target "$BINARY_TARGET_TRIPLE" \
-        -p modality-probe-debug-collector --bin modality-probe-debug-collector
-
-    mkdir -p "$PACKAGE_PATH/bin"
-    cp -a "target/$BINARY_TARGET_TRIPLE/release/modality-probe" \
-        "$PACKAGE_PATH/bin/"
-    cp -a "target/$BINARY_TARGET_TRIPLE/release/modality-probe-udp-collector" \
-        "$PACKAGE_PATH/bin/"
-    cp -a "target/$BINARY_TARGET_TRIPLE/release/modality-probe-debug-collector" \
-        "$PACKAGE_PATH/bin/"
-    cp -a "target/$BINARY_TARGET_TRIPLE/release/modality-probe-offline-batch-collector" \
-        "$PACKAGE_PATH/bin/"
-
-    $LLVM_STRIP --strip-unneeded --strip-debug "$PACKAGE_PATH/bin/modality-probe"
-
-    $LLVM_STRIP --strip-unneeded --strip-debug "$PACKAGE_PATH/bin/modality-probe-udp-collector"
-
-    $LLVM_STRIP --strip-unneeded --strip-debug "$PACKAGE_PATH/bin/modality-probe-debug-collector"
-
-    $LLVM_STRIP --strip-unneeded --strip-debug "$PACKAGE_PATH/bin/modality-probe-offline-batch-collector"
-
-    chmod 755 "$PACKAGE_PATH/bin/"*
 )
 
 # Build the libraries
@@ -160,54 +104,6 @@ TARBALL_FILE="${PACKAGE_NAME}.tar.gz"
     cp -a CHANGELOG.md "$PACKAGE_PATH/doc/"
 
     chmod 644 "$PACKAGE_PATH/doc/"*
-)
-
-# Build the man pages
-(
-    cd "$WORKSPACE_ROOT"
-
-    man_dir="$PACKAGE_PATH/man1"
-    mkdir -p "$man_dir"
-
-    help2man --version-string="$VERSION" --no-info "target/release/modality-probe" \
-        > "$man_dir/modality-probe.1"
-    help2man --version-string="$VERSION" --no-info "target/release/modality-probe-udp-collector" \
-        > "$man_dir/modality-probe-udp-collector.1"
-    help2man --version-string="$VERSION" --no-info "target/release/modality-probe-debug-collector" \
-        > "$man_dir/modality-probe-debug-collector.1"
-    help2man --version-string="$VERSION" --no-info "target/release/modality-probe-offline-batch-collector" \
-        > "$man_dir/modality-probe-offline-batch-collector.1"
-
-    gzip --no-name --best "$man_dir/modality-probe.1"
-    gzip --no-name --best "$man_dir/modality-probe-udp-collector.1"
-    gzip --no-name --best "$man_dir/modality-probe-debug-collector.1"
-    gzip --no-name --best "$man_dir/modality-probe-offline-batch-collector.1"
-
-    chmod 644 "$man_dir/"*
-)
-
-# Build the completions
-(
-    cd "$WORKSPACE_ROOT"
-
-    comp_dir="$PACKAGE_PATH/completions"
-    mkdir -p "$comp_dir"
-
-    cargo run --release \
-        -p modality-probe-cli --bin modality-probe-completions
-    cargo run --release \
-        -p modality-probe-udp-collector --bin modality-probe-udp-collector-completions
-    cargo run --release \
-        -p modality-probe-debug-collector --bin modality-probe-debug-collector-completions
-    cargo run --release \
-        -p modality-probe-offline-batch-collector --bin modality-probe-offline-batch-collector-completions
-
-    mv modality-probe.bash "$comp_dir/"
-    mv modality-probe-udp-collector.bash "$comp_dir/"
-    mv modality-probe-debug-collector.bash "$comp_dir/"
-    mv modality-probe-offline-batch-collector.bash "$comp_dir/"
-
-    chmod 644 "$comp_dir/"*
 )
 
 # Copy CMake scripts
